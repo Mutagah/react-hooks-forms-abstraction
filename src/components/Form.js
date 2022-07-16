114fdd01b8646c09d4915d5fce890022f122806a
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-16 07:39:49 +0300

    Abstracted the function and states

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,21 +1,38 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName : "John",
+    lastName : "Henry",
+    admin : false
+  })
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleChange(event) {
+    const name = event.target.name
+    let value = event.target.value
+    if(event.target.type === "checkbox")
+    {
+      value = event.target.checked
+    }
+    setFormData({
+      //copying all key value pairs from formData object
+      ...formData,
+      //overwriting the lastName key with a new value
+      [name] : value})
   }
-
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleSubmit(event){
+    event.preventDeafault();
+    console.log(formData)
   }
 
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+    <form onSubmit={handleSubmit}>
+      {/* The name in our input fields have to match the keys in our state
+          The values can now be accessed as event.target.name
+      */}
+      <input type="text" onChange={handleChange} name="firstName" value={formData.firstName} />
+      <input type="text" onChange={handleChange} name="lastName" value={formData.lastName} />
+      <input type="checkbox" name="admin" onChange={handleChange} checked={formData.admin} />
       <button type="submit">Submit</button>
     </form>
   );
